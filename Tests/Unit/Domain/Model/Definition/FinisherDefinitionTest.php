0d02b112c95534b60fe1a51ede0761f856701746
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2016-10-15 18:39:25 +0200

    chore(Travis): Add composer test script

---
+++ Tests/Unit/Domain/Model/Definition/FinisherDefinitionTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace PackageFactory\AtomicFusion\Forms\Tests\Unit\Domain\Model\Definition;
+
+use TYPO3\Flow\Tests\UnitTestCase;
+
+class FinisherDefinitionTest extends UnitTestCase {
+
+    /**
+     * @test
+     */
+    public function firstTestMethod() {
+        $this->assertTrue(true);
+    }
+
+}
