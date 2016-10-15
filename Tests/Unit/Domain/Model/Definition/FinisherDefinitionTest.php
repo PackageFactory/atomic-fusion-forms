648d0af851df396ef6026a2072cc02ec1005dae6
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2016-10-15 19:17:28 +0200

    chore(Tests): Add FinisherDefinitionTest

--- Tests/Unit/Domain/Model/Definition/FinisherDefinitionTest.php
+++ Tests/Unit/Domain/Model/Definition/FinisherDefinitionTest.php
@@ -2,14 +2,34 @@
 namespace PackageFactory\AtomicFusion\Forms\Tests\Unit\Domain\Model\Definition;
 
 use TYPO3\Flow\Tests\UnitTestCase;
+use PackageFactory\AtomicFusion\Forms\Domain\Model\Definition\FinisherDefinition;
 
 class FinisherDefinitionTest extends UnitTestCase {
 
     /**
      * @test
      */
-    public function firstTestMethod() {
-        $this->assertTrue(true);
+    public function deliversImplementationClassName() {
+        $finisherDefinition = new FinisherDefinition('SomeClassName', []);
+
+        $this->assertEquals('SomeClassName', $finisherDefinition->getImplementationClassName());
     }
 
+    /**
+     * @test
+     */
+    public function deliversOptions()
+    {
+        $finisherDefinition = new FinisherDefinition('SomeClassName', [
+            'option1',
+            'option2',
+            'option3'
+        ]);
+
+        $this->assertEquals([
+            'option1',
+            'option2',
+            'option3'
+        ], $finisherDefinition->getOptions());
+    }
 }
