7b1487554944123967f98dc5134fb23353736481
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2016-10-15 19:21:26 +0200

    chore(Tests): Add ValidatorDefinitionTest

---
+++ Tests/Unit/Domain/Model/Definition/ValidatorDefinitionTest.php
@@ -0,0 +1,56 @@
+<?php
+namespace PackageFactory\AtomicFusion\Forms\Tests\Unit\Domain\Model\Definition;
+
+use TYPO3\Flow\Tests\UnitTestCase;
+use PackageFactory\AtomicFusion\Forms\Domain\Model\Definition\ValidatorDefinition;
+
+class ValidatorDefinitionTest extends UnitTestCase {
+
+    /**
+     * @test
+     */
+    public function deliversImplementationClassName() {
+        $validatorDefinition = new ValidatorDefinition('SomeClassName', []);
+
+        $this->assertEquals('SomeClassName', $validatorDefinition->getImplementationClassName());
+    }
+
+    /**
+     * @test
+     */
+    public function deliversOptions()
+    {
+        $validatorDefinition = new ValidatorDefinition('SomeClassName', [
+            'option1',
+            'option2',
+            'option3'
+        ]);
+
+        $this->assertEquals([
+            'option1',
+            'option2',
+            'option3'
+        ], $validatorDefinition->getOptions());
+    }
+
+    /**
+     * @test
+     */
+    public function hasNoCustomErrorMessageInitially()
+    {
+        $validatorDefinition = new ValidatorDefinition('SomeClassName', []);
+
+        $this->assertFalse($validatorDefinition->hasCustomErrorMessage());
+    }
+
+    /**
+     * @test
+     */
+    public function deliversCustomErrorMessage()
+    {
+        $validatorDefinition = new ValidatorDefinition('SomeClassName', []);
+        $validatorDefinition->setCustomErrorMessage('SomeErrorMessage');
+
+        $this->assertEquals('SomeErrorMessage', $validatorDefinition->getCustomErrorMessage());
+    }
+}
