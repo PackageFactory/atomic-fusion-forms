d78ba1924c0e159544fd12267e4c27a90dc1ad30
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2016-10-16 17:38:30 +0200

    chore(Tests): Add ProcessorResolverTest

---
+++ Tests/Unit/Domain/Service/Resolver/ProcessorResolverTest.php
@@ -0,0 +1,67 @@
+<?php
+namespace PackageFactory\AtomicFusion\Forms\Tests\Unit\Domain\Service\Resolver;
+
+use TYPO3\Flow\Object\ObjectManagerInterface;
+use TYPO3\Flow\Reflection\ReflectionService;
+use PackageFactory\AtomicFusion\Forms\Domain\Service\Resolver\ProcessorResolver;
+use PackageFactory\AtomicFusion\Forms\Domain\Model\Definition\ProcessorDefinitionInterface;
+use PackageFactory\AtomicFusion\Forms\Domain\Model\Processors\ProcessorInterface;
+
+class ProcessorResolverTest extends ResolverTestCase
+{
+    const RESOLVER_TARGET_CLASS = ProcessorInterface::class;
+
+    /**
+     * @test
+     */
+    public function deliversProcessorObjectAccordingToProcessorDefinition()
+    {
+        $processorDefinition = $this->createMock(ProcessorDefinitionInterface::class);
+        $processorDefinition->method('getImplementationClassName')->willReturn('SomeProcessor');
+
+        $processorResolver = new ProcessorResolver();
+        $this->injectObjectManager($processorResolver, ['SomeProcessor']);
+
+        $processor = $processorResolver->resolve($processorDefinition);
+
+        $this->assertEquals('SomeProcessor', $processor->getClassNameForTestPurposes());
+    }
+
+    /**
+     * @test
+     * @expectedException \PackageFactory\AtomicFusion\Forms\Domain\Exception\ResolverException
+     * @expectedExceptionCode 1476599710
+     */
+    public function complainsIfRequestedProcessorDoesNotExist()
+    {
+        $processorDefinition = $this->createMock(ProcessorDefinitionInterface::class);
+        $processorDefinition->method('getImplementationClassName')->willReturn('SomeProcessor');
+        $processorDefinition->method('getOptions')->willReturn([]);
+
+        $processorResolver = new ProcessorResolver();
+        $this->injectObjectManager($processorResolver, []);
+
+        $processor = $processorResolver->resolve($processorDefinition);
+
+        $this->assertEquals('SomeProcessor', $processor->getClassNameForTestPurposes());
+    }
+
+    /**
+     * @test
+     * @expectedException \PackageFactory\AtomicFusion\Forms\Domain\Exception\ResolverException
+     * @expectedExceptionCode 1476599826
+     */
+    public function complainsIfRequestedProcessorDoesNotImplementProcessorInterface()
+    {
+        $processorDefinition = $this->createMock(ProcessorDefinitionInterface::class);
+        $processorDefinition->method('getImplementationClassName')->willReturn('SomeProcessor');
+        $processorDefinition->method('getOptions')->willReturn([]);
+
+        $processorResolver = new ProcessorResolver();
+        $this->injectObjectManager($processorResolver, [], ['SomeProcessor']);
+
+        $processor = $processorResolver->resolve($processorDefinition);
+
+        $this->assertEquals('SomeProcessor', $processor->getClassNameForTestPurposes());
+    }
+}
---
+++ Tests/Unit/Domain/Service/Resolver/ResolverTestCase.php
@@ -0,0 +1,72 @@
+<?php
+namespace PackageFactory\AtomicFusion\Forms\Tests\Unit\Domain\Service\Resolver;
+
+use TYPO3\Flow\Tests\UnitTestCase;
+use TYPO3\Flow\Object\ObjectManagerInterface;
+use TYPO3\Flow\Reflection\ReflectionService;
+
+abstract class ResolverTestCase extends UnitTestCase
+{
+    /**
+     * Inject an object manager that will always return the string class name for the requested
+     * instance.
+     *
+     * @param mixed $resolver
+     * @param array $validClassNames
+     * @param array $invalidClassNames
+     * @return void
+     */
+    protected function injectObjectManager(
+        $resolver,
+        array $validClassNames = [],
+        array $invalidClassNames = []
+    )
+    {
+        $reflectionService = $this->createMock(ReflectionService::class);
+        $objectManager = $this->createMock(ObjectManagerInterface::class);
+        $returnMap = [];
+
+        $returnMap[] = [ReflectionService::class, $reflectionService];
+
+        if (is_a(current($validClassNames), static::RESOLVER_TARGET_CLASS)) {
+            $reflectionService->method('getAllImplementationClassNamesForInterface')
+                ->with(static::RESOLVER_TARGET_CLASS)->willReturn(array_keys($validClassNames));
+        } else {
+            $reflectionService->method('getAllImplementationClassNamesForInterface')
+                ->with(static::RESOLVER_TARGET_CLASS)->willReturn($validClassNames);
+        }
+
+        foreach ($validClassNames as $key => $value) {
+            if (is_a($value, static::RESOLVER_TARGET_CLASS)) {
+                $instance = $value;
+                $validClassName = $key;
+            } else {
+                $instance = $this->getMockBuilder(static::RESOLVER_TARGET_CLASS)
+                    ->setMethods(array_merge(
+                        ['getClassNameForTestPurposes'],
+                        get_class_methods(static::RESOLVER_TARGET_CLASS)
+                    ))
+                    ->getMock();
+                $instance->method('getClassNameForTestPurposes')->willReturn($value);
+
+                $validClassName = $value;
+            }
+
+            $returnMap[] = [$validClassName, $instance];
+        }
+
+        foreach ($invalidClassNames as $invalidClassName) {
+            $returnMap[] = [$invalidClassName, new \stdClass];
+        }
+
+        $objectManager->method('isRegistered')->will($this->returnCallback(
+            function ($name) use ($validClassNames, $invalidClassNames) {
+                return in_array($name, $validClassNames) || array_key_exists($name, $validClassNames) ||
+                    in_array($name, $invalidClassNames);
+            }
+        ));
+        $objectManager->method('get')->will($this->returnValueMap($returnMap));
+
+        $this->inject($resolver, 'objectManager', $objectManager);
+    }
+}
