72b5e7d744659ffc7cb0b9c5546a0b8207b0afb6
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2016-10-29 21:42:13 +0200

    feat(FusionObjects): Add FinisherCollectionImplementation

---
+++ Classes/PackageFactory/AtomicFusion/Forms/Fusion/Finishers/FinisherCollectionImplementation.php
@@ -0,0 +1,62 @@
+<?php
+namespace PackageFactory\AtomicFusion\Forms\Fusion\Finishers;
+
+/**
+ * This file is part of the PackageFactory.AtomicFusion.Forms package
+ *
+ * (c) 2016 Wilhelm Behncke <wilhelm.behncke@googlemail.com>
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\TypoScript\TypoScriptObjects\AbstractTypoScriptObject;
+use PackageFactory\AtomicFusion\Forms\Domain\Model\Definition\FinisherDefinitionInterface;
+
+/**
+ * Fusion object to create finisher definitions from collections
+ */
+class FinisherCollectionImplementation extends AbstractTypoScriptObject
+{
+    /**
+     * Returns a list of finisher definitions
+     *
+     * @return array<FinisherDefinitionInterface>
+     */
+    public function evaluate()
+    {
+        $result = [];
+        $collection = $this->tsValue('collection');
+        $itemName = $this->tsValue('itemName');
+
+        foreach ($collection as $item) {
+            $finisherDefinition = $this->renderFinisherDefinition($itemName, $item);
+
+            $result[$finisherDefinition->getName()] = $finisherDefinition;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Render a single finisher definition
+     *
+     * @param string $itemName
+     * @param mixed $item
+     * @return FinisherDefinitionInterface
+     */
+    protected function renderFinisherDefinition($itemName, $item)
+    {
+        $this->tsRuntime->pushContextArray($this->tsRuntime->getCurrentContext() + [
+            $itemName => $item
+        ]);
+
+        $finisherDefinition = $this->tsRuntime->evaluate(sprintf('%s/finisherRenderer', $this->path), $this);
+
+        $this->tsRuntime->popContext();
+
+        return $finisherDefinition;
+    }
+}
---
+++ Tests/Unit/Fusion/Finishers/FinisherCollectionImplementationTest.php
@@ -0,0 +1,61 @@
+<?php
+namespace PackageFactory\AtomicFusion\Forms\Tests\Unit\Fusion\Finishers;
+
+use TYPO3\Flow\Tests\UnitTestCase;
+use TYPO3\TypoScript\Core\Runtime as FusionRuntime;
+use PackageFactory\AtomicFusion\Forms\Domain\Model\Definition\FinisherDefinitionInterface;
+use PackageFactory\AtomicFusion\Forms\Fusion\Finishers\FinisherCollectionImplementation;
+
+class FinisherCollectionImplementationTest extends UnitTestCase
+{
+    /**
+     * @test
+     */
+    public function createsListsOfFinisherDefinitions()
+    {
+        $fusionRuntime = $this->createMock(FusionRuntime::class);
+        $finisherListImplementation = new FinisherCollectionImplementation($fusionRuntime, '', '');
+
+        $finisherDefinition1 = $this->createMock(FinisherDefinitionInterface::class);
+        $finisherDefinition2 = $this->createMock(FinisherDefinitionInterface::class);
+
+        $fusionRuntime->method('getCurrentContext')->willReturn([]);
+
+        $fusionRuntime->expects($this->exactly(4))
+            ->method('evaluate')
+            ->withConsecutive(
+                ['/collection', $finisherListImplementation],
+                ['/itemName', $finisherListImplementation],
+                ['/finisherRenderer', $finisherListImplementation],
+                ['/finisherRenderer', $finisherListImplementation]
+            )
+            ->will($this->onConsecutiveCalls(
+                ['Item1', 'Item2'],
+                'TheItemName',
+                $finisherDefinition1,
+                $finisherDefinition2
+            ));
+
+        $fusionRuntime->expects($this->exactly(2))
+            ->method('pushContextArray')
+            ->withConsecutive(
+                [['TheItemName' => 'Item1']],
+                [['TheItemName' => 'Item2']]
+            );
+
+        $fusionRuntime->expects($this->exactly(2))->method('popContext');
+
+        $finisherDefinition1->expects($this->once())
+            ->method('getName')
+            ->willReturn('finisherName1');
+
+        $finisherDefinition2->expects($this->once())
+            ->method('getName')
+            ->willReturn('finisherName2');
+
+        $finisherDefinitionMap = $finisherListImplementation->evaluate();
+
+        $this->assertSame($finisherDefinition1, $finisherDefinitionMap['finisherName1']);
+        $this->assertSame($finisherDefinition2, $finisherDefinitionMap['finisherName2']);
+    }
+}
